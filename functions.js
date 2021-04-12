743f357221e5540b473a75f95a5f07f21dea79a1
Zedonk1995 <paulRenglish@yahoo.co.uk>
2021-04-12 16:22:55 +0100

    added a comment to the startGame function

--- functions.js
+++ functions.js
@@ -1,6 +1,6 @@
 
 
-
+//hides the main menu and begins the game.
 const startGame = () => {
     //document.getElementById('menu div id').style.display = 'none'
     //document.getElementById('game div id').style.display = 'block'
