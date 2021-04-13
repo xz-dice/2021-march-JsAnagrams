9191e7dcafb7adfa11801d2b86760f3440e9732b
Zedonk1995 <paulRenglish@yahoo.co.uk>
2021-04-13 14:44:38 +0100

    deleted whitespace

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 let countries = []
 
-
-
 fetch('countries.json')
     .then(countryData => countryData.json())
     .then(data => {
         countries = data
     })
-
-
-
-
 
 document.getElementById('startButton').addEventListener('click', () => {
     if(countries.length > 0)
