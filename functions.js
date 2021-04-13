3b405cf60a41cfa6300a45db1e2e839a234aa5b0
Zedonk1995 <paulRenglish@yahoo.co.uk>
2021-04-13 11:43:10 +0100

    trying to understand how whether the start button event listener should be inside or outside the fetch function

--- functions.js
+++ functions.js
@@ -1,10 +1,9 @@
 //hides the main menu and begins the game.
 const startGame = () => {
-    //document.getElementById('instructionScreen').style.display = 'none'
-    //document.getElementById('mainScreen').style.display = 'block'
-    //let randomCountry = getRandomCountry()
-    //let formattedCountry = formatCountry(randomCountry)
-    //displayCountry(randomCountry)
-    console.log('yo')
-
+    document.getElementById('instructionScreen').style.display = 'none'
+    document.getElementById('mainScreen').style.display = 'block'
 }
+
+//let randomCountry = getRandomCountry()
+//let formattedCountry = formatCountry(randomCountry)
+//displayCountry(randomCountry)
--- index.js
+++ index.js
@@ -2,9 +2,11 @@ fetch('countries.json')
     .then(countryData => countryData.json())
     .then(data => {
         console.log(data)
+
     })
 
 
-
-
-document.getElementById('test').addEventListener('click',startGame)
+document.getElementById('startButton').addEventListener('click', () => {
+    startGame()
+    //displayCountry(countries)
+})
