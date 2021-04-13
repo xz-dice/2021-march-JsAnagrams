b7604a47d18a7a83728c9d45dde77bffdafc95ef
khorh <helen.khor@googlemail.com>
2021-04-13 12:39:43 +0100

    update to add format country code

--- functions.js
+++ functions.js
@@ -8,3 +8,20 @@ const getRandomCountry = (countries) => {
     const randomIndex = Math.floor(Math.random() * countries.length)
     return countries.splice(randomIndex, 1)[0]
 }
+
+
+//split the random country into individual characters
+let country = 'India';
+
+let formatCountry = () => {
+    let lowercaseSplitCountry = country.toLowerCase().split('');
+        for (let i = lowercaseSplitCountry.length - 1; i > 0; i--) {
+            let j = Math.floor(Math.random() * (i + 1));
+            let temp = lowercaseSplitCountry[i];
+            lowercaseSplitCountry[i] = lowercaseSplitCountry[j];
+            lowercaseSplitCountry[j] = temp;
+        }
+        return lowercaseSplitCountry.join()
+}
+
+console.log(formatCountry());
