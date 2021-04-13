6debaa55f280202418d2589c5cf70dbd69f769db
onekatytocode <1katytogo@gmail.com>
2021-04-13 09:35:30 +0100

    updated function

--- functions.js
+++ functions.js
@@ -1,11 +1,35 @@
-let countries = [
+// let countries = [
+//     'india',
+//     'iceland',
+//     'united kingdom',
+//     'japan',
+//     'spain'
+// ]
+
+//export version
+// const countries = () => {
+//     console.log(countries.length) // Output: 5
+// //create a random number to start to select a country
+//     let getRandomNumber = Math.floor(Math.random() * countries.length)
+//     console.log(getRandomNumber)
+// }
+// export { countries }
+//=================================================================================
+
+//refactored version
+//create an array with fake list of countries
+
+const countries = [
     'india',
     'iceland',
     'united kingdom',
     'japan',
     'spain'
 ];
+
 console.log(countries.length); // Output: 5
 //create a random number to start to select a country
-let getRandomNumber = Math.floor(Math.random() * countries.length);
+const getRandomNumber = Math.floor(Math.random() * countries.length);
 console.log(getRandomNumber);
+//select one country from the array at random
+console.log(countries[getRandomNumber]);
