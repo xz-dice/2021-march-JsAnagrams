606de7537e036ead3956774f22a01175c0939d94
MattW-R <Matthew.wr89@gmail.com>
2021-04-15 12:03:21 +0100

    Fixed displayGameOver doc block

--- functions.js
+++ functions.js
@@ -182,7 +182,7 @@ const startTimer = (timerObject, score) => {
  * function to change from main screen to game over screen and display final score,
  * the timer object's interval is also cleared
  * @param {number} score the player's score
- * @param {interval} interval the timer object's interval property
+ * @param interval the timer object's interval property
  */
 const displayGameOver = (score, interval) => {
     document.getElementById('mainScreen').style.display = 'none'
